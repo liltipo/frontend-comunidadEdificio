608da56695035117ec54c2a2bc6eb86cdf476008
liltipo <alvaro.opazos@usm.cl>
2024-11-12 15:31:13 -0300

    new-home

--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -1,28 +1,55 @@
 import React from 'react';
-
-import edificio from '../assets/edificio-home.jpg'
-import columpios from '../assets/columpios-home.jpg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBuilding} from '@fortawesome/free-solid-svg-icons'
+import edificio from '../assets/edificio-home.jpg';
+import columpios from '../assets/columpios-home.jpg';
+import quincho from '../assets/quincho.png'; 
+import terraza from '../assets/terraza.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBuilding } from '@fortawesome/free-solid-svg-icons';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
 
 const HomePage = () => {
   return (
     <div className="home-container">
-      <div className="home-left">
-        <img src={edificio} alt="Imagen edificio" />
-        <p>Mantenemos la gestión financiera de la comunidad organizada y accesible. Aquí podrás realizar el pago de los gastos comunes de forma online, evitando trámites presenciales. Además, tendrás acceso a tus historiales de pago y facturas, todo desde la comodidad de tu hogar. <br /> <br /> La transparencia es uno de nuestros pilares fundamentales. Queremos que estés siempre informado sobre en qué se utilizan los fondos recaudados por los gastos comunes. En la sección de "Boletín", publicamos informes periódicos detallando el estado financiero de la comunidad, los proyectos en los que se invierte el dinero y cualquier otro tema relevante para los residentes.</p>
+      <div className="home-header">
+        <img src={edificio} alt="Imagen edificio" className="edificio-image" />
+        <div className="home-title">
+          <h1>
+            <FontAwesomeIcon icon={faBuilding} size="lg" style={{ marginRight: '10px' }} />
+            COMUNIDAD EDIFICIO <span>*PLACE HOLDER*</span>
+          </h1>
+        </div>
       </div>
-      <div className="home-right">
-      <h1>
-        <FontAwesomeIcon icon={faBuilding} size="sm" style={{ marginRight: '10px' }} />
-        COMUNIDAD EDIFICIO
-        <br />
-        <span> *PLACE HOLDER* </span>
-      </h1>
-      <p>
-En esta plataforma dedicada a la comunidad de Placeholder, podrás gestionar y acceder a todos los servicios relacionados con la vida en nuestro edificio. Nos enfocamos en mejorar la calidad de vida de nuestros residentes, proporcionando herramientas que facilitan la administración de los espacios compartidos y la transparencia en la gestión de los recursos comunes.
-<br /> <br />Nuestro edificio cuenta con varios espacios comunes como el quincho y la terraza, perfectos para reuniones familiares, eventos sociales o simplemente disfrutar al aire libre. A través de esta página, podrás reservar estos espacios de manera rápida y sencilla, eligiendo el día y la hora que mejor se ajusten a tus necesidades.</p>
-        <img src={columpios} alt="Imagen columpios" />
+
+      <div className="home-description">
+        <p>
+          Gestionamos de manera eficiente los espacios compartidos de la comunidad, brindándote la posibilidad de reservar áreas como el quincho o la terraza de forma rápida y sencilla. Además, mantenemos una total transparencia en la gestión financiera: podrás consultar en qué se destinan los fondos recaudados a través de informes detallados.
+          <br /> <br />
+          También podrás realizar el pago de tus gastos comunes de manera online, sin necesidad de trámites presenciales, y acceder a tu historial de pagos y facturas directamente desde esta plataforma, todo diseñado para tu comodidad y conveniencia.
+        </p>
+      </div>
+
+      <div className="home-carousel">
+      <Carousel 
+          showThumbs={false} 
+          autoPlay 
+          infiniteLoop={true} /* Asegura que el bucle infinito está activado */
+          centerMode={true} /* Centra el carrusel */
+          centerSlidePercentage={70} /* Muestra el 70% de la imagen activa */
+          interval={3000} /* Controla el intervalo de auto deslizamiento */
+          emulateTouch={true} /* Permite control táctil (swipe en dispositivos móviles) */
+          showStatus={false} /* Oculta el contador de imágenes */
+        >
+          <div className="carousel-item">
+            <img src={columpios} alt="Imagen columpios" />
+          </div>
+          <div className="carousel-item">
+            <img src={quincho} alt="Imagen quincho" />
+          </div>
+          <div className="carousel-item">
+            <img src={terraza} alt="Imagen terraza" />
+          </div>
+        </Carousel>
       </div>
     </div>
   );
